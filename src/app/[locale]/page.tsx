21b52ed70836e3cf513ed269b5510261d3629c81
Alessio Moioli <alessio.moioli@paramount.com>
2026-05-13 15:07:00 +0200

    hide github stats integration

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,14 +4,10 @@ import Certifications from '@/components/Certifications'
 import Skills from '@/components/Skills'
 import Experience from '@/components/Experience'
 import Projects from '@/components/Projects'
-import GitHubStats from '@/components/GitHubStats'
 import Writing from '@/components/Writing'
 import Contact from '@/components/Contact'
-import { fetchGitHubStats } from '@/lib/github'
 
-export default async function LocalePage() {
-  const githubStats = await fetchGitHubStats()
-
+export default function LocalePage() {
   return (
     <main id="main-content">
       <Hero />
@@ -20,7 +16,6 @@ export default async function LocalePage() {
       <Skills />
       <Experience />
       <Projects />
-      {githubStats && <GitHubStats stats={githubStats} />}
       <Writing />
       <Contact />
     </main>
